b7b667d927cadfa06127d9397df9ed106cb03f89
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-08 16:16:25 -0500

    update events with api call

--- Cohort2/team6/client/src/pages/Events/Events.tsx
+++ Cohort2/team6/client/src/pages/Events/Events.tsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
 import Ticket from "./components/Ticket";
 import dummyData from "./dummydata";
-import { Text } from "@chakra-ui/react";
+import { Box, Spinner, Text } from "@chakra-ui/react";
+import { EventTypes } from "../../types/Event.types";
+import axios, { AxiosResponse } from "axios";
+import { API_URL } from "../../constants/api-constants";
 
 const Events = () => {
+  const [events, setEvents] = useState<EventTypes[] | null>(null);
+
+  //get events api call
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response: AxiosResponse<EventTypes[]> = await axios.get<
+          EventTypes[]
+        >(`${API_URL}/api/events`);
+        console.log(response.data);
+        setEvents(response.data.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData(); // Call the async function to fetch data
+  }, []);
   return (
     <>
       <Text
@@ -13,9 +35,28 @@ const Events = () => {
         justifyContent={"center"}
         alignItems={"center"}
       >
-        Events
+        Events 
       </Text>{" "}
-      <Ticket dummyData={dummyData} />
+      <Box
+        h={events ? "" : "82vh"}
+        display={events ? "" : "flex"}
+        justifyContent={events ? "" : "center"}
+        alignItems={events ? "" : "center"}
+      >
+        {events ? (
+          <Ticket dummyData={events} />
+        ) : (
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            width="220px"
+            height={"220px"}
+            justifySelf={"center"}
+          />
+        )}
+      </Box>
     </>
   );
 };
